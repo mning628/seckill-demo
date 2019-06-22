f74a2a61b326cd999eeb112de9687eb3bdd5475f
mning628 <975670918@qq.com>
2019-06-22 21:49:15 +0800

    修改代码格式

--- src/main/java/com/example/demo/SpringBootSecondsKillApplication.java
+++ src/main/java/com/example/demo/SpringBootSecondsKillApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecondsKillApplication {
+public class SpringBootSecondsKillApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecondsKillApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootSecondsKillApplication.class, args);
+    }
 
 }
